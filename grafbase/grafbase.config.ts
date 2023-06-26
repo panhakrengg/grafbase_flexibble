97f04d3b0a057780bf15bfb9b1774f6ba1ea2e94
Krengpanha <panha.kreng@khalibre.com>
2023-06-26 21:11:19 +0700

    Init interface

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,27 @@
+import { g, auth, config } from '@grafbase/sdk';
+
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string(),
+  githubUrl: g.url().optional(),
+  projects: g
+    .relation(() => Project)
+    .list()
+    .optional(),
+});
+
+const Project = g.model('Project', {
+  title: g.string(),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string(),
+  createBy: g.relation(() => User),
+});
+
+export default config({
+  schema: g,
+});
